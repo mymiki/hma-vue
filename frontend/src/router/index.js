7fe58b7a57809a74ef3d901e0a99d926492f4c9b
sjh860811 <sunjinghao@300.cn>
2020-08-18 15:58:19 +0800

    2

---
+++ frontend/src/router/index.js
@@ -0,0 +1,122 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import MenuView from '@/views/common/MenuView'
+import PageView from '@/views/common/PageView'
+import LoginView from '@/views/login/Common'
+import EmptyPageView from '@/views/common/EmptyPageView'
+import HomePageView from '@/views/HomePage'
+import db from 'utils/localstorage'
+import request from 'utils/request'
+
+// 全局Router异常处理
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location) {
+  // return originalPush.call(this, location).catch(err => { if (typeof err !== 'undefined')console.log(err) })
+  // 改写为try catch包裹的方式 see https://stackoverflow.com/questions/56438111/how-to-fix-error-typeerror-cannot-read-property-catch-of-undefined-when-s
+  // 引用一段话.catch can be used on promise. as you are not returning any promise here(or anything at all), its like using .catch on undefined. Use try catch instead
+  try {
+    return originalPush.call(this, location)
+  } catch (err) { if (typeof err !== 'undefined')console.log(err) }
+}
+Vue.use(Router)
+
+let constRouter = [
+  {
+    path: '/login',
+    name: '登录页',
+    component: LoginView
+  },
+  {
+    path: '/index',
+    name: '首页',
+    redirect: '/home'
+  }
+]
+
+let router = new Router({
+  routes: constRouter
+})
+
+const whiteList = ['/login']
+
+let asyncRouter
+
+// 导航守卫，渲染动态路由
+router.beforeEach((to, from, next) => {
+  if (whiteList.indexOf(to.path) !== -1) {
+    next()
+  }
+  let token = db.get('USER_TOKEN')
+  let user = db.get('USER')
+  let userRouter = get('USER_ROUTER')
+  if (token.length && user) {
+    if (!asyncRouter) {
+      if (!userRouter) {
+        request.get(`menu/${user.username}`).then((res) => {
+          asyncRouter = res.data
+          save('USER_ROUTER', asyncRouter)
+          go(to, next)
+        }).catch(err => { console.error(err) })
+      } else {
+        asyncRouter = userRouter
+        go(to, next)
+      }
+    } else {
+      next()
+    }
+  } else {
+    next('/login')
+  }
+})
+
+function go (to, next) {
+  asyncRouter = filterAsyncRouter(asyncRouter)
+  router.addRoutes(asyncRouter)
+  next({...to, replace: true})
+}
+
+function save (name, data) {
+  localStorage.setItem(name, JSON.stringify(data))
+}
+
+function get (name) {
+  return JSON.parse(localStorage.getItem(name))
+}
+
+function filterAsyncRouter (routes) {
+  return routes.filter((route) => {
+    let component = route.component
+    if (component) {
+      switch (route.component) {
+        case 'MenuView':
+          route.component = MenuView
+          break
+        case 'PageView':
+          route.component = PageView
+          break
+        case 'EmptyPageView':
+          route.component = EmptyPageView
+          break
+        case 'HomePageView':
+          route.component = HomePageView
+          break
+        default:
+          route.component = view(component)
+      }
+      if (route.children && route.children.length) {
+        route.children = filterAsyncRouter(route.children)
+      }
+      return true
+    }
+  })
+}
+
+function view (path) {
+  return function (resolve) {
+    import(`@/views/${path}.vue`).then(mod => {
+      resolve(mod)
+    })
+  }
+}
+
+export default router
